e297e926b00a8142f27e681a60a37ce0d804bdd4
Khuat Van Dung <khuat.van.dung@framgias-MacBook-Pro-6.local>
2017-09-21 09:23:54 +0700

    init project

--- DemoOpenURL/ViewController.swift
+++ DemoOpenURL/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let latitude = 21.023369
+    let longitude = 105.790073
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tapped(_ sender: Any) {
+        //Working in Swift new versions.
+        if UIApplication.shared.canOpenURL(URL(string: "comgooglemaps://")!)
+        {
+            let urlString = "http://maps.google.com/?daddr=\(latitude),\(longitude)&directionsmode=driving"
+        
+            // use bellow line for specific source location
+        
+            //let urlString = "http://maps.google.com/?saddr=\(sourceLocation.latitude),\(sourceLocation.longitude)&daddr=\(destinationLocation.latitude),\(destinationLocation.longitude)&directionsmode=driving"
+        
+            UIApplication.shared.openURL(URL(string: urlString)!)
+        } else
+        {
+            //let urlString = "http://maps.apple.com/maps?saddr=\(sourceLocation.latitude),\(sourceLocation.longitude)&daddr=\(destinationLocation.latitude),\(destinationLocation.longitude)&dirflg=d"
+            let urlString = "http://maps.apple.com/maps?daddr=\(latitude),\(longitude)&dirflg=d"
+        
+            UIApplication.shared.openURL(URL(string: urlString)!)
+        }
+
+
+    }
 
 }
-
